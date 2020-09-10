d7c1d8e9cc86716d745f30c960aabb7ffd3cb9c4
AndreiSimkin <62541520+AndreiSimkin@users.noreply.github.com>
2020-09-10 10:39:03 +0300

    Добавьте файлы проекта.

---
+++ SpellСhecker/Dictonory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpellСhecker
+{
+    public class Dictonory
+    {
+        public class Letter
+        {
+            public Letter(char character)
+            {
+                Сharacter = character;
+                Letters = new List<Letter>();
+            }
+
+            public char Сharacter { get; set; }
+            List<Letter> Letters { get; set; }
+        }
+    }
+}
---
+++ SpellСhecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpellСhecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
